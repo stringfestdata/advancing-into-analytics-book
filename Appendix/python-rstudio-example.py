7db6a5516f5885047c3360e0aa914c6ced893fd9
George Mount <gxm204@case.edu>
2020-10-19 20:57:26 -0400

    appendix

---
+++ Appendix/python-rstudio-example.py
@@ -0,0 +1,16 @@
+# To start a Python script in RStudio, 
+# go to File -> New File -> Python script.
+
+# You can now execute lines from Python just
+# as you would with R.
+ 
+ 
+a = 1
+a
+
+my_list = [1,2,3,'Boo!']
+my_list[0]
+
+# Setting up packages is a bit more involved, check out 
+# https://support.rstudio.com/hc/en-us/articles/360023654474-Installing-and-Configuring-Python-with-RStudio
+
