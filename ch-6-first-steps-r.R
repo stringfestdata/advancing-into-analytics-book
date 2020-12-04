c36deb93f4a5f1a4a89f2bf66d8d4dc967a66cc9
George Mount <gxm204@case.edu>
2020-12-03 20:24:57 -0500

    update files

--- ch-6-first-steps-r.R
+++ ch-6-first-steps-r.R
@@ -1,6 +1,3 @@
-1 * 2
-3 + 5 - 4
-
 ?sqrt
 
 require(stats) # for spline
@@ -9,15 +6,32 @@ xx <- -9:9
 plot(xx, sqrt(abs(xx)),  col = "red")
 lines(spline(xx, sqrt(abs(xx)), n=101), col = "pink")
 
-
 # Multiplication before addition
 3 * 5 + 6 
 
 # Division before subtraction
 2 / 2 - 7
 
-# What is the absolute value of -100?
 
+1 * 2 # This comment is possible
+
+# This comment is preferred
+2 * 1
+
+
+# What is the absolute value of -100?
+abs(-100)
+
+
+# These aren't going to work!
+ABS(-100)
+Abs(-100)
+
+# Convert to upper case
+toupper('I love R')
+
+# Is 3> 4?
+3 > 4
 
 # Assigning an object in R
 my_first_object = abs(-100)
@@ -26,49 +40,62 @@ my_first_object = abs(-100)
 # Printing an object in R
 my_first_object
 
-my_second_object <- sqrt(abs(-5^2))
+
+my_second_object <- sqrt(abs(-5 ^ 2))
 my_second_object
 
+my_char <- 'Hello, world' 
+my_other_char <- "We're able to code R!" 
+my_num <- 3
+my_int <- 12L 
+my_logical <- F 
 
-my_char <- 'Hello, world'
-my_num <- 3.2
-my_int <- 12L
-my_logical <- F
 
-
-typeof(my_char)
-typeof(my_num)
-typeof(my_int)
-typeof(my_logical)
-
+str(my_char)
+str(my_num)
+str(my_int)
+str(my_logical)
 
 # Is my_num equal to 5.5?
 my_num == 5.5
 
 # Number of characters in my_char
-# (This is like `LEN()` in Excel)
 nchar(my_char)
+
 
 my_other_num <- 2.2
 my_num <- my_num/my_other_num
 my_num
 
 
-# Run this once
-# install.packages('tidyverse')
+install.packages('tidyverse')
 
 ?arrange
+
 arrange(mtcars, cyl, disp)
 
 library(tidyverse)
 
-arrange(mtcars, cyl, disp)
+?arrange(mtcars, cyl, disp)
 
-
-
+update.packages()
 
 
 install.packages('installr')
 library(installr)
-help(package='installr')
+updateR()
+
+install.packages('devtools') 
+library(devtools) 
+install_github('andreacirilloac/updateR') 
+library(updateR) 
+updateR(admin_password = 'Admin user password') 
+
+
+
+
+
+
+
+
 
